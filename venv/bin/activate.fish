939fc6157cc9f68057a636a1f41414154d276ee1
Alexey Popov <aleshaka@inbox.ru>
2023-03-09 12:41:52 +0300

    changing the structure to classes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/popovalexey/PycharmProjects/character_creation_module/venv"
+set -gx VIRTUAL_ENV "/Users/popovalexey/PycharmProjects/practicum/character_creation_module/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
